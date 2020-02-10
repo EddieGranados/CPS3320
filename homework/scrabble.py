8769e11c4207a2af51d9a80513325654b5bfd3ff
Eddie Granados <egranados04@live.com>
2020-02-10 12:22:58 -0500

    Updated Scrabble Game

--- homework/scrabble.py
+++ homework/scrabble.py
@@ -1,8 +1,7 @@
 
 #scarrble letter dictionary
-scrabble_tiles = {'A':1,'B':3,'C':3,'D':2,'E':1,'F':4,'G':2,'H':4,'I':1,
-'J':8,'K':5,'L':1,'M':3,'N':1,'O':1,'P':3,'Q':10,'R':1,'S':1,'T':1,'U':1,
-'V':4,'W':4,'X':8,'Y':4,'Z':10}
+scrabble_tiles = {'A':1,'B':3,'C':3,'D':2,'E':1,'F':4,'G':2,'H':4,'I':1,'J':8,'K':5,'L':1,'M':3,
+                  'N':1,'O':1,'P':3,'Q':10,'R':1,'S':1,'T':1,'U':1,'V':4,'W':4,'X':8,'Y':4,'Z':10}
 
 def letterScore(n):  # create a function that takes a single letter
     global letter 
@@ -17,11 +16,11 @@ def wordScore(n):  # create a function that inputs a single word/string
     word = str(n.upper()) #set up word
     score = 0
 
-    for i in range(0,len(word)):
+    for i in range(0,len(word)): #search through range of the length of the word
         if (word[i] != ' '):
             letterScore(word[i])
             score = score + scrabble_tiles[letter]
     print("Word: %s\nScore: %d" % (word, score))
     
-wordScore("hello")
+wordScore("heLLo")
 #wordScore("  -!@#   word  fwdjfhs")
