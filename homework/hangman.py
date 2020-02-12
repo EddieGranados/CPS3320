fc66f7f97136ef0a44beb44247033d2d8736c8d2
Eddie Granados <egranados04@live.com>
2020-02-11 22:16:06 -0500

    Most recent hangman code - 2/11/2020

--- homework/hangman.py
+++ homework/hangman.py
@@ -1,37 +1,53 @@
 # Hangman game!
 
-# Assume the answer is "hangman"
+import random
 
-A = ['h', 'a', 'n', 'g', 'm', 'a', 'n']
-L = ['_', '_', '_', '_', '_', '_', '_']
+play = True
+chances = 0
 
-attempts = 0
+A = [] #Answer List
+L = [] #Input List
+#list of words to choose from
+word_list = ['hangman', 'anthropology', 'nose', 'gaurd', 'mother', 'awesome', 'north']
+
+#randomly assign a word to A, and make the word a list
+A = list(random.choice(word_list))
+
+#for 0 to the length of A, at index x, add an underscore to L
+for x in range(0,len(A)):
+    L.insert(x,'_')
 
 while play == True:
     # Ask the user to guess a letter
     letter = str(input("Guess a letter: "))
 
+    #if the input that is entered is nothing, make it an underscore
+    if (letter == ''):
+        letter = '_'
+
     # Check to see if that letter is in the Answer
-    i = 0
-    
-    for currentletter in A:
+    if (letter not in A[:] or letter == ''):
+        print("BAD GUESS!")
+        chances = chances + 1
+        print("YOU HAVE %d CHANCES LEFT!" % (6-chances))
+
+    for currentletter in A[:]:
         # If the letter the user guessed is found in the answer,
         # set the underscore in the user's answer to that letter
         if (letter in currentletter):
-            L[i] = letter
+            L[currentletter.index()] = letter
 
-        i = i + 1
-
-
-
-    
     # Display what the player has thus far (L) with a space
     # separating each letter
     print(' '.join(str(n) for n in L))
 
     # Test to see if the word has been successfully completed,
     # and if so, end the loop
-    if A == L:
+    if (chances == 6):
         play = False
+        print("BETTER LUCK NEXT TIME!")
 
-print("GREAT JOB!")
+    if (L == A):
+        play = False
+        print("GREAT JOB!")
+print(len(A))
