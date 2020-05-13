a02c816a27c6e7e462cedfa5916dcfec3eae3468
Eddie Granados <egranados04@live.com>
2020-05-13 17:48:39 -0400

    Final Project

---
+++ finalProject/covid19FAQ/config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+class DefaultConfig:
+    # """ Bot Configuration """
+
+    # PORT = 3978
+    # APP_ID = os.environ.get("MicrosoftAppId", "")
+    # APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    
+    """ Bot Configuration """
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+
+    QNA_KNOWLEDGEBASE_ID = os.environ.get(
+        "QnAKnowledgebaseId", "3274199d-4739-4dfa-b21b-ffdc4afe6b7d")
+    QNA_ENDPOINT_KEY = os.environ.get(
+        "QnAEndpointKey", "cb523b38-0d94-4600-a423-58b2061c9e0f")
+    QNA_ENDPOINT_HOST = os.environ.get(
+        "QnAEndpointHostName", "https://myfirstqnaeg.azurewebsites.net/qnamaker")
