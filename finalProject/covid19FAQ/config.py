6434b4581b206ea5160ce897e64eacb41eee13c2
Eddie Granados <egranados04@live.com>
2020-05-13 17:50:00 -0400

    Update

--- finalProject/covid19FAQ/config.py
+++ finalProject/covid19FAQ/config.py
@@ -17,8 +17,8 @@ class DefaultConfig:
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
 
     QNA_KNOWLEDGEBASE_ID = os.environ.get(
-        "QnAKnowledgebaseId", "3274199d-4739-4dfa-b21b-ffdc4afe6b7d")
+        "QnAKnowledgebaseId", "")
     QNA_ENDPOINT_KEY = os.environ.get(
-        "QnAEndpointKey", "cb523b38-0d94-4600-a423-58b2061c9e0f")
+        "QnAEndpointKey", "")
     QNA_ENDPOINT_HOST = os.environ.get(
-        "QnAEndpointHostName", "https://myfirstqnaeg.azurewebsites.net/qnamaker")
+        "QnAEndpointHostName", "")
